8ef47ee2b6e3dc43a99029692cd893f8e5796010
Saalim <saalim520>
2023-09-01 22:23:24 +0530

    OK

---
+++ Mern Udemy/1-node-farm/starter/index.js
@@ -0,0 +1,18 @@
+const { isUtf8 } = require("buffer");
+const fs = require("fs");
+const http = require("http");
+
+// console.log("hello brother");
+// const txt = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(txt);
+
+// const textout = "good boy";
+
+// fs.writeFileSync("./txt/output.txt", textout);
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("hello From server");
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to req on port 8000");
+});
