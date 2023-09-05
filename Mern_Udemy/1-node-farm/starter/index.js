5cdd1951f1f365fbbdbbd7e801ac1c89d4ea2568
Saalim Muzaffar <saalim520@gmail.com>
2023-09-05 23:30:26 +0530

    changes

--- Mern_Udemy/1-node-farm/starter/index.js
+++ Mern_Udemy/1-node-farm/starter/index.js
@@ -4,13 +4,47 @@ const http = require("http");
 const { dirname } = require("path");
 const { json } = require("stream/consumers");
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not - organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") res.end("This is overview");
-  else if (pathName == "/api") {
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHtml);
+    res.end(output);
+  } else if (pathName == "/api") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(data);
   }
