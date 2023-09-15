0e0905a0f9ad57fb8cf96cc65a47bf85b105f7bf
Saalim Muzaffar <saalim520@gmail.com>
2023-09-15 22:52:12 +0530

    ok

---
+++ RJS/newsapp/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return <div>Hello My First Class Based Component</div>;
+  }
+}
